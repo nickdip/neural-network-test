c5b1b7eda26e24744f3f8be87eee4e9ca766501a
Joe Bailey <joebailey1000@hotmail.co.uk>
2023-11-03 09:19:35 +0000

    improved algorithm generates an expected score based on normal distribution of values

--- regession_test.py
+++ regession_test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import exp, sqrt, pi
+from math import sqrt
 
 #Danceability
 #Energy
@@ -55,6 +55,7 @@ songs_matrix2 = np.array([[1,0.1, 0.1, 0.1],
                           [0.1, 0.97, 0.92, 0.95],
                           [0.1, 0.99, 0.95, 0.96]])
 
+
 user_likes2 = [1, 0.1, 0.1, 0.1]
 
 new_song2 = [0.9, 0.1, 0.1, 0.1] # issue with singular matrices
@@ -62,30 +63,35 @@ new_song2 = [0.9, 0.1, 0.1, 0.1] # issue with singular matrices
 # print(predict_score(songs_matrix, user_likes, new_song, 0))
 print(predict_score(songs_matrix2, user_likes2, new_song2, 0))
 
-songs_matrix3 = np.array([[1,0.1, 0.1, 0.1],
-                          [0.1, 0.98, 0.9, 0.98],
-                          [0.1, 0.97, 0.92, 0.95],
-                          [0.1, 0.99, 0.95, 0.96]])
+songs_matrix3 = (
+    (0.5, 0.5, 0.5, 0.5),
+    (0.59, 0.51, 0.55, 0.7),
+    (0.99, 0.99, 0.9, 0.92),
+    (0.05, 0.1, 0.1, 0.1),
+    (0.72, 0.38, 0.91, 0.44),
+    (0.58, 0.99, 0.37, 0.26)
+)
+user_likes2 = [10, 10, 1, 1, 6, 7]
+new_song2 = [0.5, 0.5, 0.8, 0.1]
 
-user_likes2 = [10, 1, 1, 1]
-
-new_song2 = [0.8, 0.2, 0.2, 0.2] # issue with singular matrices
-
-scores = [list(a[0]) for a in list(zip([a for a in songs_matrix3]))]
+print('here')
+scores = (a for a in zip(*songs_matrix3))
 print(scores)
 
 
-def count_up(scores,likes):
+def count_up(scores, likes):
     table = []
     for i in enumerate(scores):
         table.append([])
+        print(i[1])
         for j in enumerate(i[1]):
-            for k in range(user_likes2[j[0]]):
+            for k in range(likes[j[0]]):
                 table[i[0]].append(i[1][j[0]])
     return table
 
 
 scores_table = count_up(scores,user_likes2)
+print('scores table')
 print(scores_table)
 
 
@@ -94,15 +100,16 @@ def get_means(table):
 
 
 means = get_means(scores_table)
-
+print('means')
 print(means)
 
 
 def get_stdevs(table, means):
-    return [sum([(score-mean[1])**2 for score in table[mean[0]]])/len(table[mean[0]]) for mean in enumerate(means)]
+    return [sum([(score-mean[1])**2 for score in table[mean[0]]])/len(user_likes2) for mean in enumerate(means)]
 
 
 stdevs = get_stdevs(scores_table, means)
+print('stdevs')
 print(stdevs)
 
 
@@ -111,7 +118,9 @@ def normal_distribution(mean , sd, x):
     return prob_density
 
 
-for i in enumerate(new_song2):
-    print(i)
-    print(normal_distribution(means[i[0]], stdevs[i[0]], i[1])/(stdevs[i[0]]*sqrt(2*pi))*8)
+expected_ratings = np.array([
+    normal_distribution(means[i[0]], stdevs[i[0]], i[1])/(stdevs[i[0]]*sqrt(2*np.pi))*8 for i in enumerate(new_song2)
+])     # this *8 normalises results to s zero to ten scale
 
+print('expected ratings')
+print(expected_ratings)
